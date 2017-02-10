223108cc8b4eac9a860c6a4b7a978b6254f099e5
reginaldo.andrade <reginaldo.andrade@outlook.com>
2017-02-10 11:10:09 -0200

    Tratamento global de exceções. Adição de scope transacional em BaseDAO

--- src/main/java/com/zup/xyapi/controller/handler/ValidationHandlerController.java
+++ src/main/java/com/zup/xyapi/controller/handler/ValidationHandlerController.java
@@ -8,6 +8,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,12 +23,14 @@ public class ValidationHandlerController {
 	@Autowired
 	private MessageSource messageSource;
 
+	private ApiResponse<Object> apiResponse;
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 	@ResponseBody
 	public ApiResponse<Object> modelValidateResponseError(MethodArgumentNotValidException ex) {
 
-		ApiResponse<Object> apiResponse = new ApiResponse<>();
+		apiResponse = new ApiResponse<>();
 		apiResponse.setMessage("");
 
 		List<FieldError> fields = ex.getBindingResult().getFieldErrors();
@@ -41,7 +44,7 @@ public class ValidationHandlerController {
 				apiResponse.setMessage(message);
 			});
 		}
-
+		
 		apiResponse.setCode(HttpStatus.BAD_REQUEST.value());
 		apiResponse.setStatus(HttpStatus.BAD_GATEWAY.getReasonPhrase());
 
@@ -62,6 +65,37 @@ public class ValidationHandlerController {
 		}
 
 		return message;
+	}
+
+	@ExceptionHandler(value = { HttpMediaTypeException.class })
+	@ResponseStatus(code = HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+	@ResponseBody
+	public ApiResponse<Object> unsuportedMediaResponseError(HttpMediaTypeException e) {
+
+		return responseError(e.getMessage(), HttpStatus.UNSUPPORTED_MEDIA_TYPE.value(),
+				HttpStatus.UNSUPPORTED_MEDIA_TYPE.getReasonPhrase());
+	}
+
+	@ExceptionHandler(value = { Exception.class, RuntimeException.class })
+	@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+	@ResponseBody
+	public ApiResponse<Object> genericResponseError(Exception e) {
+		
+		return responseError(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(),
+				HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+
+	}
+
+	private ApiResponse<Object> responseError(String message, int statusCode, String statusMessage) {
+
+		apiResponse = new ApiResponse<>();
+		apiResponse.setMessage(message);
+
+		apiResponse.setCode(statusCode);
+		apiResponse.setStatus(statusMessage);
+
+		return apiResponse;
+
 	}
 
 }
--- src/main/java/com/zup/xyapi/dao/BaseDAO.java
+++ src/main/java/com/zup/xyapi/dao/BaseDAO.java
@@ -6,7 +6,11 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional
 public abstract class BaseDAO<T> {
 	
 	@Autowired
